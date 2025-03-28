70a02de8538e260d35c43773ffe2f3937bd9f8f6
SolennEstier <solenn.estier@gmail.com>
2025-03-28 22:40:30 +0100

    modifier mon level test

--- minijam181/scripts/level_test_solenn.gd
+++ minijam181/scripts/level_test_solenn.gd
@@ -1,14 +1,28 @@
 extends Node2D
 
-@onready var enemy: AnimatableBody2D = $Enemy
+@onready var enemy_1: AnimatableBody2D = $Enemy1
+@onready var enemy_2: AnimatableBody2D = $Enemy2
+@onready var enemy_3: AnimatableBody2D = $Enemy3
+
+var onoff : int = 1
 
 func _ready() -> void:
-	enemy.turning = true
-
+	#enemy.turning = true
+	pass
+	
 func _on_level_timer_timeout() -> void:
 	#enemy.scale.x = -1
-	#enemy.rotation_degrees += 90
-	pass
+	if onoff == 1 :
+		enemy_1.rotation_degrees += 180
+		enemy_2.rotation_degrees += 180
+		enemy_3.rotation_degrees += 180
+		onoff = 0
+	else :
+		enemy_1.rotation_degrees -= 180
+		enemy_2.rotation_degrees -= 180
+		enemy_3.rotation_degrees -= 180
+		onoff = 1
+	
 
 func _on_enemy_hit() -> void:
 	print("Aie")
