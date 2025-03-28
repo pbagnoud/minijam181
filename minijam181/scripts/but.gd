27e33b07344cda6f32812467168fd1ff727daa79
SolennEstier <solenn.estier@gmail.com>
2025-03-28 20:59:31 +0100

    Added goal

--- minijam181/scripts/but.gd
+++ minijam181/scripts/but.gd
@@ -1,1 +1,7 @@
-extends Node2D
+extends Area2D
+
+signal goal
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		goal.emit()
--- minijam181/scripts/main.gd
+++ minijam181/scripts/main.gd
@@ -1,1 +1,11 @@
 extends Node2D
+
+
+func _on_goal_goal() -> void:
+	print("FINI")
+	# Terminer la scene et passer au niveau suivant
+
+
+func _on_enemy_hit() -> void:
+	print("HIT")
+	# Terminer la scène et indiquer la défaite, proposer de recommencer ?
