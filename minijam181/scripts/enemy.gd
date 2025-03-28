9b67e763e3751896e8ea578aec9ad124c34d8504
SolennEstier <solenn.estier@gmail.com>
2025-03-28 22:22:37 +0100

    added start menu

--- minijam181/scripts/enemy.gd
+++ minijam181/scripts/enemy.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 signal hit
 
+var angular_speed = PI/2
+
+@export var turning : bool = false
 
 func flip_up() :
 	scale.x = -1
@@ -15,3 +18,7 @@ func _on_enemy_detection_zone_body_entered(body: Node2D) -> void:
 func _on_vision_cone_area_2_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player") :
 		hit.emit()
+		
+func _physics_process(delta: float) -> void:
+	if turning : 
+		rotation += angular_speed * delta
--- minijam181/scripts/level_test_solenn.gd
+++ minijam181/scripts/level_test_solenn.gd
@@ -2,11 +2,13 @@ extends Node2D
 
 @onready var enemy: AnimatableBody2D = $Enemy
 
-
+func _ready() -> void:
+	enemy.turning = true
 
 func _on_level_timer_timeout() -> void:
-	enemy.scale.x = -1
-
+	#enemy.scale.x = -1
+	#enemy.rotation_degrees += 90
+	pass
 
 func _on_enemy_hit() -> void:
 	print("Aie")
