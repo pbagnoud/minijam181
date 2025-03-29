da9524761942fe760d13c998d1d7b2d9a52fade1
SolennEstier <solenn.estier@gmail.com>
2025-03-29 18:58:11 +0100

    Added poison mechanics

--- minijam181/scripts/level_2.gd
+++ minijam181/scripts/level_2.gd
@@ -1,6 +1,8 @@
 extends Node2D
 @onready var player: CharacterBody2D = $Player
 @onready var hint_timer: Timer = $HintTimer
+@onready var death_screen: ColorRect = $DeathScreen
+
 
 
 func _on_rabbit_1_friend_found(type) -> void:
@@ -14,3 +16,18 @@ func _on_goal_goal() -> void:
 
 func _on_enemy_hit() -> void:
 	print("Hit")
+
+
+func _on_poison_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		died_of_poison()
+		#if Player.type != 1 :
+		#	died_of_poison()
+		pass
+			
+func died_of_poison() :
+	death_screen.visible = true
+
+
+func _on_restart_button_pressed() -> void:
+	get_tree().reload_current_scene()
