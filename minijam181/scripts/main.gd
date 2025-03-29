025be5c175a9f5f4d4d77ba89fc2dfc182e3d12c
SolennEstier <solenn.estier@gmail.com>
2025-03-29 16:22:46 +0100

    tried and failed to change menu

--- minijam181/scripts/main.gd
+++ minijam181/scripts/main.gd
@@ -11,3 +11,6 @@ func _on_goal_goal() -> void:
 func _on_enemy_hit() -> void:
 	print("HIT")
 	# Terminer la scène et indiquer la défaite, proposer de recommencer ?
+
+func _on_next_level_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level2.tscn")
--- minijam181/scripts/rabbit.gd
+++ minijam181/scripts/rabbit.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 signal friend_found
 
+@export var type : int = 0
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		friend_found.emit()
