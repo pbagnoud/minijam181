ceca130b7ceb808960ff68731979eefb9968bd1c
VB <119983496+vibonnier@users.noreply.github.com>
2025-03-29 15:10:57 +0100

    grid mouv player

--- minijam181/scripts/player.gd
+++ minijam181/scripts/player.gd
@@ -1,11 +1,19 @@
-extends CharacterBody2D
+extends Area2D
 
-var speed = 300
+var tile_size = 64
+var inputs = {"ui_right": Vector2.RIGHT,
+			"ui_left": Vector2.LEFT,
+			"ui_up": Vector2.UP,
+			"ui_down": Vector2.DOWN}
 
-func get_input():
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = input_dir * speed
+func _ready():
+	position = position.snapped(Vector2.ONE * tile_size)
+	position += Vector2.ONE * tile_size/2
+	
+func _unhandled_input(event):
+	for dir in inputs.keys():
+		if event.is_action_pressed(dir):
+			move(dir)
 
-func _physics_process(delta):
-	get_input()
-	move_and_collide(velocity * delta)
+func move(dir):
+	position += inputs[dir] * tile_size
